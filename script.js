073c9e4e112cbc2961987d610d90d774ee7d033f
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2022-07-13 11:13:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const countDate = new Date("jan 1, 2023 00:00:00").getTime();
+
+function newYear() {
+  let now = new Date().getTime();
+  let gap = countDate - now;
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
+  let d = Math.floor(gap / (day));
+  let h = Math.floor((gap % (day)) / (hour));
+  let m = Math.floor((gap % (hour)) / (minute));
+  let s = Math.floor((gap % (minute)) / (second));
+  document.getElementById("day").innerText = d;
+  document.getElementById("hour").innerText = h;
+  document.getElementById("minute").innerText = m;
+  document.getElementById("second").innerText = s;
+};
+setInterval(() => newYear(), 1000);
